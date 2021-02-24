fad7fa38b6633eafcc802d4a00ac6c834c52e6b3
Lucas Campelo <campelo.lucas@hotmail.com>
2021-02-24 13:26:22 -0300

    Terceiro dia do NLW#4

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,14 @@
+import { ChallengesContext } from 'contexts/ChallengesContext'
+import { useContext } from 'react'
 import styles from '../styles/components/CompletedChallenges.module.css'
 
 export function CompletedChallenges() {
+  const { challengesCompleted } = useContext(ChallengesContext);
+
   return (
     <div className={styles.completedChallangesContainer}>
       <span>Desafios completos</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   )
 }
