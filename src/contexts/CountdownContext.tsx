9617bb527a704e0fa224316cdcbc9e95395d96b9
Lucas Campelo <campelo.lucas@hotmail.com>
2021-02-25 14:22:16 -0300

    mend

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -18,10 +18,10 @@ export const CountdownContext = createContext({} as CountdownContextData);
 
 let countdownTimeout: NodeJS.Timeout;
 
-export function CountdownProvider({children}: CountdownProviderProps) {
+export function CountdownProvider({ children }: CountdownProviderProps) {
     const { startNewChallenge } = useContext(ChallengesContext);
 
-    const [time, setTime] = useState(0.05*60);
+    const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
@@ -30,26 +30,26 @@ export function CountdownProvider({children}: CountdownProviderProps) {
 
     function startCountdown() {
         setIsActive(true);
-      }
-    
-      function resetCountdown() {
+    }
+
+    function resetCountdown() {
         clearTimeout(countdownTimeout);
         setIsActive(false);
-        setTime(0.05*60);
+        setTime(25 * 60);
         setHasFinished(false);
-      }
-    
-      useEffect(() => {
+    }
+
+    useEffect(() => {
         if (isActive && time > 0) {
-          countdownTimeout = setTimeout(() => {
-            setTime(time-1);
-          }, 1000)
+            countdownTimeout = setTimeout(() => {
+                setTime(time - 1);
+            }, 1000)
         } else if (isActive && time === 0) {
-          setHasFinished(true);
-          setIsActive(false);
-          startNewChallenge();
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
         }
-      }, [isActive, time]);
+    }, [isActive, time]);
 
     return (
         <CountdownContext.Provider value={{
